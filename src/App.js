3071a247defccd168e900f82893786dc8c286a1a
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-08-05 13:26:31 +0200

    return to normal start

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 function App(){
 
-    const title = 'Blog Post'
-    const body = 'This is my Blog post'
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-        {id: 4, text: 'Comment four'}
-    ]
+    return(
+        <div className="container">
+            <h1>My App</h1>
 
-    const loading = false
-    const showComment = true
+        </div>
+    )
 
-    if(loading) return <h1> Loading...</h1>
-
-    const commentBlock = (<div className ="comments">
-    <h3> Comments ({comments.length})</h3>
-    <ul>
-        {comments.map((comment,index) => (
-            <li key = {index}> {comment.text} </li>
-        ))}
-    </ul>
-</div>)
-
-    return (
-    <div className="container">
-        <h1>My App</h1>
-        <h1> { title.toUpperCase()}</h1>      
-        <p> {body}</p>     
-        {showComment && commentBlock}
-    </div>        
-    ) // looks like html but its actually JSX
 }
 
 export default App
