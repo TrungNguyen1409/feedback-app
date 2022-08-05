6b1a6bf066146dee8fc359d101fe0b921fd47d22
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-08-05 14:33:52 +0200

    add feedback card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header"
+import FeedbackItem from "./components/FeedbackItem"
 
 function App(){
 
@@ -6,7 +7,7 @@ function App(){
         <>
             <Header/>
             <div className="container">
-                <h1>My App</h1>
+                <FeedbackItem />
             </div>
         </>
         
