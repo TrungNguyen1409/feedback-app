f756c247b0e42ba2a73370e7aa9f8f2e0d721aa0
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-08-08 11:13:19 +0200

    display rating and number of review

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 function FeedbackStats({feedback}) {
+
+  
+  let average = feedback.reduce((acc,cur)=>{
+    return (acc + cur.rating)
+  }, 0) / feedback.length
+
+  console.log(feedback)
+  console.log(feedback.length)
+  console.log(average)
   return (
     <div className='feedback-stats'>
         
         <h4>{feedback.length} Review</h4>
-        <h4>Average Rating: {feedback.forEach}</h4>
+        <h4>Average Rating: {isNaN(average) ? 0: average}</h4>
         
     Stats
     </div>
