6208e7f75ea096b7f5fba37b5a0e2ef6f7937755
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-08-08 14:39:59 +0200

    realtime validation for text field (only enable the button when there are more than 10 chars)

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -8,8 +8,20 @@ import Button from './shared/Button'
 function FeedbackForm() {
 
     const [text,setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        if(text===''){
+            setBtnDisabled(true)
+            setMessage(null)
+        }else if(text !== '' && text.trim().length <= 10){
+            setMessage('Text must be at least 10 Characters')
+            setBtnDisabled(true)
+        }else{
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setText(e.target.value)
     }
   return <Card>
@@ -24,8 +36,9 @@ function FeedbackForm() {
                 placeholder='Write a review'
                 value={text}
             />
-            <Button type="submit">Send</Button>
+            <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
+        {message && <div className='message'>{message}</div>}
     </form>
   </Card>
 }
